f5b966bcd8b2e30bc8615d992aabbd997fe6f59f
gavin-rilee <you@example.com>
2021-06-07 20:48:56 -0400

    Final

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,72 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const Account = require("./accounts-model");
+const {
+  checkAccountId,
+  checkAccountNameUnique,
+  checkAccountPayload,
+} = require("./accounts-middleware");
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get("/", async (req, res, next) => {
+  try {
+    const data = await Account.getAll();
+    res.json(data);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+router.get("/:id", checkAccountId, (req, res, next) => {
+  res.json(req.account);
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
-})
+router.post(
+  "/",
+  checkAccountPayload,
+  checkAccountNameUnique,
+  async (req, res, next) => {
+    try {
+      const name = req.body.name.trim();
+      const budget = req.body.budget;
+      const newAccount = await Account.create({ name, budget });
+      res.status(201).json(newAccount);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.put(
+  "/:id",
+  checkAccountId,
+  checkAccountPayload,
+  checkAccountNameUnique,
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const updatedAccount = await Account.updateById(id, req.body);
+      res.json(updatedAccount);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.delete("/:id", checkAccountId, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedAccount = await Account.deleteById(id);
+    res.json(deletedAccount);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
 
 module.exports = router;
